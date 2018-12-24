1a32596bc4c7ab5851780e3712feb7f0256dc35f
oainin <postmaster@oainin.com>
2018-12-24 14:06:18 +0800

    2018年12月24日14:06:15

--- js/face.js
+++ js/face.js
@@ -1,17 +1,1 @@
-var video = document.getElementById('pv'), canvas = document
-				.getElementById('pca'), img = document.getElementById('pim'), videourl = window.URL
-				|| window.webkitURL, index;
-
-		//媒体对象
-		navigator.getMedia = navigator.getUserMedia
-				|| navagator.webkitGetUserMedia || navigator.mozGetUserMedia
-				|| navigator.msGetUserMedia;
-		navigator.getMedia({
-			video : true,
-			audio : false
-		}, function(strem) {
-			
-			video.src = videourl.createObjectURL(strem);
-		}, function(error) {
-			console.log(error);
-		});
+$('.photovideo').html("<img src='img/IMG_1390.JPG' width='100%' height='100%' >");
--- js/pc.js
+++ js/pc.js
@@ -3,16 +3,35 @@ $(function(){
 					$('#mainpage').slideDown(1000);
 					
 				},1000);
+	             setTimeout(function() {
+	             	$.ajax({
+	             		url:'http://io.oainin.com/loginOver',
+	             		type:'get',
+	             		async:true,
+	             		success:function(msg){
+	             			if(msg[0]){
+	             				window.location.href='core.html';
+	             			}
+	             			else{
+	             				console.log(msg[0]);
+	             			}
+	             		}
+	             	});
+	             },1000);
+				
+				
 				setInterval(function(){
 					if($('.username').val()!='' && $('.password').val()!=''){
 						$('.password').blur(function(){
 							$.ajax({
-								url:'io.oainin.com/loginDo',
+								url:'http://io.oainin.com/loginDo',
 								type:'get',
 								async:true,
 								data:'username='+$('.username').val()+'&password='+$('.password').val(),
 								success:function(msg){
-									console.log(msg);
+									if(msg[0].username!=null){
+										window.location.href='core.html';
+									}
 								}
 							});
 						});
